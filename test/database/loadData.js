0add00a77ed138975cb51e54a03fb2c6f5eabc2a
emckean <emckean@Erins-MacBook-Pro-2.local>
2016-12-27 15:36:55 -0800

    updates circle.yml, adds file for loading test data

---
+++ test/database/loadData.js
@@ -0,0 +1,37 @@
+use test
+
+var adoption = {
+    "fullName": "Testy McTesterson",
+    "email": "michaeldayreads@alphaorder.com",
+    "word": "empiricism",
+    "wordHash": "5857087ff36d2873dac58e77",
+    "status": "active",
+    "dateAdded": "Sun Dec 18 2016 15:43:37 GMT+0000 (UTC)",
+    "note": "",
+    "Twitter": "",
+    "type": "paid",
+    "earlyAdopter": "",
+    "paidStatus": "paid",
+    "promoCode": "",
+    "offensive": "FALSE",
+    "giftee": "",
+    "gifteeEmail": "",
+    "gifteeNote": "",
+    "gifteeTwitter": "",
+    "gifteeLink": "",
+    "WordnikUsername": "",
+    "backerType": "ADOPTER",
+    "adoptLink": "",
+    "renewal": "false",
+    "dateExpires": "Mon 19 Dec 2017 06:46:45 GMT",
+    "payment": [
+      {
+        "date": "Sun Dec 18 2016 15:43:37 GMT+0000 (UTC)",
+        "type": "Stripe",
+        "amount": "25.00"
+      }
+    ],
+    "paypal-url": ""
+  }
+
+db.adoptions.insert(adoption);
