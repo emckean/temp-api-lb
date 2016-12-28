7e078af62d4ce30ccbd390775e78b60beae91245
emckean <emckean@Erins-MacBook-Pro-2.local>
2016-12-27 16:15:50 -0800

    adds supertest test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 "use strict";
 
 var expect = require ('chai').expect;
+var supertest = require('supertest');
+var api = supertest('http://localhost:3000/api')
+
 
 describe ('canary test', function (){
 	it ('should pass this canary test', function (){
 		expect(true).to.be.true;
 	});
+});
+
+describe ('get all test', function (){
+
+ it('returns an adopted word', function(done) {
+    api.get('/adoptedWords')
+    .expect(200, done);
+  });
+
 });
