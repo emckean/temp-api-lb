2d902cee1a70ca05cd167bfaf15a7d3807110116
emckean <emckean@Erins-MacBook-Pro-2.local>
2017-01-09 13:59:03 -0800

    testing output of env variable

--- test/database/teardownBluemixMongo.js
+++ test/database/teardownBluemixMongo.js
@@ -11,9 +11,10 @@ var cfenv = require('cfenv');
 // localVCAP = require("../local-vcap.json")
 // var appEnv = cfenv.getAppEnv({vcap: localVCAP})
 var appEnv = process.env.appEnv;
-console.log(appEnv)
+
 // Within the application environment (appenv) there's a services object
 var services = appEnv.services;
+console.log(services)
 // The services object is a map named by service so we extract the one for mongo
 var mongodb_services = services["compose-for-mongodb"];
 console.log(mongodb_services)
