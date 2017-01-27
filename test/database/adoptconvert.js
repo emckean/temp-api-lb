8b39f6601d49c61f542027c64c9c24931388500a
emckean <emckean@Erins-MacBook-Pro-2.local>
2017-01-27 13:36:18 -0600

    wrangles more with promises

--- test/database/adoptconvert.js
+++ test/database/adoptconvert.js
@@ -2,6 +2,7 @@
 console.log('converting data')
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
+var async = require('async')
 
 var validUrl = require('valid-url');
 var url = require('url');
@@ -13,14 +14,28 @@ console.log(newUrl);
 var url = 'mongodb://localhost:27017/test';
 // var testData = require('./testdata.json')
 
-var testData = require('/Users/emckean/Documents/datasets/adopt/01192017-edit.json')
+var formats = ['ddd MMM d YYYY HH:mm:ss a', 'ddd, d MMM YYYY HH:mm:ss z', 'YYYY-MM-DDTHH:mm:ssZ' ]
 
-function fixTwitter(adoptObj, callback){
-  if (adoptObj.hasOwnProperty('Twitter') && adoptObj.Twitter === 'None'){
-    adoptObj.Twitter = ''
-  }
-  callback(null, adoptObj)
-} 
+//don't forget that this file isn't clean JSON and needs line-end commas and square brackets
+var testData = require('/Users/emckean/Documents/datasets/adopt/01192017.json')
+var cleanTestData = [];
+
+function selectValues(topLevelObj){
+   return new Promise(function(resolve, reject) {
+  // console.log(topLevelObj.path.collection + ' ' + topLevelObj.kind)
+  if (topLevelObj.path.collection === "adoptedWords" && topLevelObj.value ){
+    resolve(topLevelObj.value)
+  } 
+})
+ }
+
+var fixTwitter = function(adoptObj) {
+  return new Promise(function(resolve, reject) {
+        if (adoptObj.hasOwnProperty('Twitter') && adoptObj.Twitter === 'None'){
+           adoptObj.Twitter = ''
+        }
+        resolve(adoptObj)
+})}
 
 function convertDate(date){
   var formats = ['ddd MMM d YYYY HH:mm:ss a', 'ddd, d MMM YYYY HH:mm:ss z', 'YYYY-MM-DDTHH:mm:ssZ' ]
@@ -29,106 +44,117 @@ function convertDate(date){
   return(ISODate)
 }
 
-function addPayment(adoptObj, callback){
-  // console.log(adoptObj.dateAdded)
-  var date = convertDate(adoptObj.dateAdded);
-  var payment = [
-    {
-      "date": date,
-      "type": "not recorded",
-      "amount": "25.00"
+var addPayment = function(adoptObj){
+  return new Promise(function(resolve, reject) {
+    var date = convertDate(adoptObj.dateAdded);
+    var payment = [
+      {
+        "date": date,
+        "type": "not recorded",
+        "amount": "25.00"
+      }
+    ]
+    if (adoptObj.hasOwnProperty('payment') && !Array.isArray(adoptObj.payment)){
+      var tempArray = adoptObj.payment
+      delete adoptObj.payment;
+      adoptObj.payment = [tempArray]
     }
-  ]
-  if (adoptObj.hasOwnProperty('payment')){
-     adoptObj.payment[0].date = convertDate(adoptObj.payment[0].date)
-  }
-  if (!adoptObj.hasOwnProperty('payment')){
-    adoptObj.payment = payment
-    if (moment(adoptObj.dateAdded).isSame('2016-01-01', 'day')){
-      adoptObj.payment[0].type = 'Kickstarter'
+    if (adoptObj.hasOwnProperty('payment')){
+       adoptObj.payment[0].date = convertDate(adoptObj.payment[0].date)
     }
-  }
-    callback(null, adoptObj)
+    if (!adoptObj.hasOwnProperty('payment')){
+      adoptObj.payment = payment
+      if (moment(adoptObj.dateAdded, formats).isSame('2016-01-01', 'day')){
+        adoptObj.payment[0].type = 'Kickstarter'
+      }
+    }
+     resolve(adoptObj)
+  })
 }
 
-function addExpire(adoptObj, callback){
-    var formats = ['ddd MMM d YYYY HH:mm:ss a', 'ddd, d MMM YYYY HH:mm:ss z', 'YYYY-MM-DDTHH:mm:ssZ' ]
+var checkDateAdded = function(adoptObj) {
+  return new Promise(function(resolve, reject) {
+    if (!adoptObj.hasOwnProperty('dateAdded')){
+      adoptObj.error = true
+      adoptObj.dateAdded = 'Fri Jan 01 2016 19:46:45 GMT-0800'
+    }
+      resolve(adoptObj)
+  })
+}
+
+var addExpire = function(adoptObj){
+    return new Promise(function(resolve, reject) {
     if (!adoptObj.hasOwnProperty('dateExpires')){
+      // console.log(adoptObj.word)
       adoptObj.dateExpires = moment(adoptObj.dateAdded, formats).add(1, 'years').format()
       // console.log(adoptObj.dateExpires)
-      callback(null, adoptObj) 
     }    
+    resolve(adoptObj)
+})
+  }
+
+
+var miscCleanup = function(adoptObj){
+   return new Promise(function(resolve, reject) {
+      delete adoptObj['paypal-url'];
+      delete adoptObj.paidStatus;
+      adoptObj._id = adoptObj.wordHash;
+      adoptObj.dateAdded = convertDate(adoptObj.dateAdded)
+      if (adoptObj.hasOwnProperty('dateRenewed')){ adoptObj.dateRenewed =convertDate(adoptObj.dateRenewed)}
+      resolve(adoptObj)
+   })
 }
 
-// testData.map(function(word){
-//   console.log(word.word)
-//   ad
-//   console.log(word)
+var cleanData = testData.map(function(obj){
+        if (selectValues(obj) !== undefined){
+          // cleanData.push(selectValues(obj))
+               return (selectValues(obj));
+        }
+    })
+
+var finalData = [];
+function makeFinalArray(word, callback){
+       miscCleanup(word)
+      .then(fixTwitter)
+      .then(addPayment)
+      .then(checkDateAdded)
+      .then(addExpire)
+      .then(function(data){
+        console.log(data)
+        return(data)
+        
+      })
+      .catch(function(error) {
+        console.log('oh no', error);
+      })
+}
+
+
+Promise.all(cleanData.map(function(word, index){
+  return makeFinalArray(word)
+  // finalData.push(word)
+
+}))
+
+// function tryThis(callback){
+//   callback(null, Promise.all(cleanData.map(function(word, index){
+//   return makeFinalArray(word)
+//   // finalData.push(word)
+
+//   })))
+// }
+
+// tryThis(function(err, response){
+//   console.log(response)
 // })
 
-// convertDate('Fri, 22 Jan 2016 03:46:45 GMT')
+// async.map(cleanData, makeFinalArray, function(err, results) {
+//     // results is now an array of stats for each file
+//     console.log(err)
+//     console.log(results[0])
+// });
+//this is coming up weird promises
 
-testData.map(function(word){
-  // console.log(word.word);
-    delete word['paypal-url'];
-    delete word.paidStatus;
-    word._id = word.wordHash;
-    word.dateAdded = convertDate(word.dateAdded)
-    if (word.hasOwnProperty('dateRenewed')){ word.dateRenewed =convertDate(word.dateRenewed)}
-    fixTwitter(word, function (err, response){
-      addPayment(response, function(err, response){
-        addExpire(response, function(err, response){
-          console.log(JSON.stringify(response, null, 2))
-          return(response)
-        })
-      })
-      
-    })
-    
-           
-});
 
-// exports.setUp = function(callback){
-//     mongoose.connect('mongodb://localhost:27017/test');
 
-//     var db = mongoose.connection;
-//     db.on('error', console.error.bind(console, 'connection error:'));
-//     db.once('open', function() {
-//         var Schema = mongoose.Schema;
-//         var wordSchema = mongoose.Schema({
-//             dateExpires: { type: Date},
-//             dateAdded: {type: Date}
-//         }, { collection: 'adoption' });
-
-//         var Adoption = mongoose.model('Adoption', wordSchema);
-//         console.log('here')
-
-//         //add callback here? 
-
-//         var newData = testData.map(function(word){
-//            word.dateAdded = new Date(word.dateAdded)
-//            if (word.dateExpires){
-//             word.dateExpires = new Date(word.dateExpires)
-//            } else {
-//               word.dateExpires = new Date(word.dateExpires)
-//            }
-//            if (word.dateRenewed){
-//             word.dateRenewed = new Date(word.dateRenewed)
-//            }
-//            word._id = word.wordHash;
-//            return (word);
-//         });
-//         Adoption.collection.insertMany(testData, function(err,r) {
-//             if (err) {
-//                 console.log(err)
-//             } else {
-//                 console.log(r.insertedCount + ' documents inserted')
-//                  callback(null, '10 documents inserted')
-//             }
-//         })
-        
-//         db.close();
-//     });
-
-// }
 
